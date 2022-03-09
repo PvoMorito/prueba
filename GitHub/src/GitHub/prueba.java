2642812b30f238e19de70a9ff30c94e4340b473f
Kevin <kevin3cl@gmail.com>
2022-03-09 13:15:04 +0100

    Esta es la segunda versión.
    He añadido un par de cosas más.
    
    Signed-off-by: Kevin <kevin3cl@gmail.com>

--- GitHub/src/GitHub/prueba.java
+++ GitHub/src/GitHub/prueba.java
@@ -6,6 +6,8 @@ public class prueba {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hola mundo");
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println("Esto es una prueba del GitHub");
 	}
 
 }
